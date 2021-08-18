9049fb4f45f1dcd9c0cdc755fe69a8b34d2655e5
Norgg <j.turner@mhnltd.co.uk>
2021-08-18 20:01:05 +0100

    Don't show code for deletion rules

--- db.py
+++ db.py
@@ -45,7 +45,11 @@ class Rule(db.Entity):
             status_string = f'{self.status}'
 
         vote_string = f'{self.yays} yay - {self.nays} nay'
-        return f'**{self.title}** ({status_string}):\n{vote_string}\n```python\n{self.code}\n```'
+        if self.deletes:
+            return f'**{self.title}**:\n{vote_string}'
+        else:
+            return f'**{self.title}** ({status_string}):\n{vote_string}\n```python\n{self.code}\n```'
+
 
     @property
     def yays(self):
